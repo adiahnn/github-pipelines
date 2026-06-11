6630a7cdf737c0fa2e757632f40d8291817ce712
Adiahn <adnanmukhtar2321@gmail.com>
2026-06-11 02:15:18 +0100

    Fixed Issues

--- src/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Devops!")
-console.log("I am learning CI/CD using github Actions")
--- src/test.js
+++
@@ -1,3 +0,0 @@
-console.log("starting tests...")
-setTimeout(() => console.log("Waiting..."), 3000)
-console.log("Test completed")
